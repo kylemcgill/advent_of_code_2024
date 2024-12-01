cfc1d3bbe0b4be425d586d2f2e1e7988e266998c
Kyle McGill <kylemcgill@Kyles-MBP.home>
2024-12-01 13:10:56 -0800

    Adding solution for Day 1 Part 2

--- 1/day1.py
+++ 1/day1.py
@@ -1,29 +1,52 @@
 import os
 
-left = []
-right = []
-with open("input.txt", "r") as f:
-    items = f.read().split()
+def get_lists():
+    left = []
+    right = []
+    with open("input.txt", "r") as f:
+        items = f.read().split()
+        
+        # white space was split between the items as well
+        toggle_left = True
+        for item in items:
+            if toggle_left:
+                left.append(int(item))
+                toggle_left = False
+            else:
+                right.append(int(item))
+                toggle_left = True
+        f.close()
+    return (left, right)
+
+def part_1():
+    left, right = get_lists()
+
+    left.sort()
+    right.sort()
+    assert(len(left) == len(right))
+
+    sum = 0
+    for i in range(len(left)):
+        sum += abs(left[i] - right[i])
+
+    print(f"Sum of distances is: {sum}")
+
+def part_2():
+    left, right = get_lists()
+
+    right_frequency = {}
+    for item in right:
+        if item not in right_frequency:
+            right_frequency[item] = 0
+        right_frequency[item] += 1
     
-    # white space was split between the items as well
-    toggle_left = True
-    for item in items:
-        if toggle_left:
-            left.append(int(item))
-            toggle_left = False
-        else:
-            right.append(int(item))
-            toggle_left = True
-    f.close()
+    similarity_score = 0
+    for item in left:
+        if item in right_frequency:
+            similarity_score += item * right_frequency[item]
 
-left.sort()
-right.sort()
-assert(len(left) == len(right))
-
-sum = 0
-for i in range(len(left)):
-    sum += abs(left[i] - right[i])
-
-print(f"Sum of distances is: {sum}")
-
+    print(f"Similarity score: {similarity_score}")
     
+if __name__ == "__main__":
+    part_1()
+    part_2()
